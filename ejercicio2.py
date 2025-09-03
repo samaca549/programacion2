af45dad0a80d50b97bd782b10ce874316dc8f03c
samaca549 <msamacar@unal.edu.co>
2025-09-03 13:35:31 -0500

    primeros ejercicios

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,10 @@
-print=("hola ya se imprimir frases")
-print=("273")
+print("hola ya se imprimir frases")
+print("273")
 numero=5.3
-print=(f"el numero decimal es {numero}")
+print(f"el numero decimal es {numero}")
+#los numeros 1234 y 532.
+numero1=1234
+numero2=532
+suma=numero1+numero2
+
+print(f"la suma de los numeros 1234 y 532 es= {suma}")
