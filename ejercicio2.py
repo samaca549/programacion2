3312c781f09eefbaa2cf79f5609d75f0b2185601
samaca549 <msamacar@unal.edu.co>
2025-09-03 13:41:39 -0500

    primeras multiplicaciones

--- ejercicio2.py
+++ ejercicio2.py
@@ -7,4 +7,17 @@ numero1=1234
 numero2=532
 suma=numero1+numero2
 
-print(f"la suma de los numeros 1234 y 532 es= {suma}")
+print(f"la suma de los numeros 1234 y 532 es = {suma}")
+
+#la resta de los numeros
+
+resta=numero1-numero2
+print(f"la resta de los numeros 1234 y 532 es ={resta}")
+
+#multiplicacion
+multiplicacion=numero1*numero2
+print(f"el resultado de la multiplicacion es ={multiplicacion}")
+
+#division
+division=numero1/numero2
+print(f"el resultado de la division es {division}")
