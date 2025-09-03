4d3eacea005a2a1441d78ae1c129f9880ff39561
samaca549 <msamacar@unal.edu.co>
2025-09-03 14:13:51 -0500

    primeros 15 ejercicios

--- ejercicio2.py
+++ ejercicio2.py
@@ -21,3 +21,71 @@ print(f"el resultado de la multiplicacion es ={multiplicacion}")
 #division
 division=numero1/numero2
 print(f"el resultado de la division es {division}")
+#con while
+numero=1
+while (numero<4):
+    print(numero)
+    numero=numero+1
+    print("el siguiente ejemplo es con for")
+#con for
+for i in range (1, 4):
+    print(i)
+print("ejemplo del 1 al 10")
+numero=1
+while(numero<10):
+    print(numero)
+    numero=numero+1
+print("el siguiente ejemplo con for")
+for i in range(1, 10):
+    print(i)
+
+print("ejercicio de 1 al 10000")
+numero=1
+while(numero<10001):
+    print(numero)
+    numero=numero+1
+for i in range(1, 10001):
+    print(i)
+
+
+print("ejercicio del 5 al 10")
+numero=5
+while(numero>4 and numero<11):
+    print(numero)
+    numero=numero+1
+
+
+for i in range(5, 11):
+    print(i)
+numero=5
+while(numero>=5 and numero<16):
+    print(numero)
+    numero=numero+1
+
+print("ejercicio con for")
+for i in range(5, 16):
+    print(i)
+
+numero=1
+while(numero>=1 and numero<15001):
+    print(numero)
+    numero=numero+1
+#ejercicio con for
+for i in range (1, 15001):
+    print(i)
+
+numero=1
+while(numero<=200):
+    print(f"hola{numero}")
+    numero=numero+1
+for i in range(1, 201):
+    print(f"hola{i}")
+#multiplicacion
+for i in range(1, 31):
+    a=i*i
+    print(f"{i}***2={a}")
+numero=1
+while (numero<=30):
+    potencia=numero*numero
+    print(f"{numero}**2={potencia}")
+    numero=numero+1
